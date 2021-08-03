e91db285b2639caed975febaeaa76787c0305064
Jose Echezria <echezuriart@gmai.com>
2021-08-03 14:22:03 -0500

    icon almost fix it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import '../styles/Navbar.css'
-import Logo from '../images/logo.svg'
+import Logo from '../images/personal-logo.svg'
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="links-wrap">
-        <img src= { Logo } alt="logo"  className="logo"/> 
+      <img src= { Logo } alt="logo" className="logo"></img>
         <a href="/home">Home</a>  
-        <a href="/aboutme">About Me</a> 
-        <a href="/contact">Contact</a>  
+        <a href="/aboutme">About</a> 
+        <a href="/contact">Contact</a>
       </div>
     </div>
   );
