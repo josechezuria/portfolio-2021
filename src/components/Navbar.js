e2a9042ee9b85c02dadadf91681c3ed43feef560
Jose Echezria <echezuriart@gmai.com>
2021-08-03 16:20:35 -0500

    header align fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,15 @@ import Logo from '../images/personal-logo.svg'
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="links-wrap">
-      <img src= { Logo } alt="logo" className="logo"></img>
+      <div className="wrapper">
+        <div className="logo">
+       <img src= { Logo } alt="logo"></img>
+      </div>
+      <div className="links-wrapper">
         <a href="/home">Home</a>  
         <a href="/aboutme">About</a> 
         <a href="/contact">Contact</a>
+      </div>
       </div>
     </div>
   );
