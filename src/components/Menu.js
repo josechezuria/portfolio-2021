abf3b2eb94ec19c703189bb7d37c10020f9b81d7
Jose Echezria <echezuriart@gmai.com>
2021-08-07 14:06:50 -0500

    Deployable Version

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,7 +8,7 @@ function Menu() {
       <p><span>Hi, I'm</span> <span className="jose">Jose Echezuria</span> <span className="webdev">Web Developer</span> <span>&</span> <span className="designer">Graphic Designer</span></p>
     </div>
     <div className="wrap-whoiam">
-      <div className="who">Hello, I am Jose Echezuria, A Branding Designer, UI/UX Designer and Web Developer, I've been working in the Design Industry since 2019.
+      <div className="who">Hello, I am Jose Echezuria, a Branding Designer, UI/UX Designer and Web Developer, I've been working in the Design Industry since 2019.
       </div>
       <div className="iam">I felt kind of incomplete a year ago, so I decided learn Web Developing to expand my knowledge about this industry and make better things.
       </div>
